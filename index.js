3f741efedc4e13e422f9e5fd715a9d96bf0e3f6c
lemon <lemon@example.com>
2024-07-19 02:07:46 +0300

    uibuild

--- index.js
+++ index.js
@@ -20,60 +20,45 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
-
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   Person.countDocuments({})
     .then(count => {
       response.send(
         `<p>Phonebook has info for ${count} people</p>
          <p>${new Date()}</p>`
       );
-    });
-});
-
-app.get('/api/persons', (request, response) => {
-  Person.find({})
-    .then(persons => {
-      response.json(persons);
     })
-    .catch(error => {
-      response.status(500).send({ error: 'Failed to retrieve data' });
-    });
+    .catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body;
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => response.json(persons))
+    .catch(error => next(error));
+});
 
-  if (!body.name || !body.number) {
+app.post('/api/persons', (request, response, next) => {
+  const { name, number } = request.body;
+
+  if (!name || !number) {
     return response.status(400).json({ error: 'name or number missing' });
   }
 
-  Person.findOne({ name: body.name })
+  Person.findOne({ name })
     .then(existingPerson => {
       if (existingPerson) {
         return response.status(400).json({ error: 'This name is already added to phonebook' });
       }
 
-      const person = new Person({
-        name: body.name,
-        number: body.number,
-      });
+      const person = new Person({ name, number });
 
       return person.save();
     })
-    .then(savedPerson => {
-      response.json(savedPerson);
-    })
-    .catch(error => {
-      console.error('Error saving person to MongoDB:', error);
-      response.status(500).send({ error: 'Failed to save person' });
-    });
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error));
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -82,46 +67,50 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end();
       }
     })
-    .catch(error => next(error))
+    .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch(error => next(error))
+    .then(() => response.status(204).end())
+    .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
-      response.json(updatedPerson)
+      if (updatedPerson) {
+        response.json(updatedPerson);
+      } else {
+        response.status(404).end();
+      }
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  } else {
+    return response.status(500).json({ error: 'Failed to retrieve data' });
+  }
 
-  next(error)
-}
+  next(error);
+};
 
-// this has to be the last loaded middleware, also all the routes should be registered before this!
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -16,11 +16,21 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
+    minLength: 3,
     required: true,
   },
   number: {
     type: String,
-    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`, 
+    },
+    required: {
+      value: true,
+      message: 'Phone number is missing!',
+    },
   },
 }, { collection: 'people' });
 
